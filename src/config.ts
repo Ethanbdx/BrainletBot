1b5ea1f1b4a1bcd744cf25b649bbbe6ef32d53b0
Ethanbdx <ethanbdx999@yahoo.com>
2019-05-23 15:09:06 -0500

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export let config = {
+    "token": "NTgwMjg1OTI4NjMxOTU5NTUy.XOTOWA.ylQ691o_4hAPytIBqqpQdxiUhi4",
+    "prefix": ".",
+    "commands": [
+        "help", /*"thanos",*/ "weebrate", "gayrate", "dankrate", "commands", "addsound", "playsound", "delsound", "listsounds"
+    ]
+}
