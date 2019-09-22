a4739a39f3943abddb2f9b7d3b291e46e114efbe
Ethan Boudreaux <ethanbdx999@yahoo.com>
2019-09-22 00:02:55 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let config = {
-    "token": "NTgwMjg1OTI4NjMxOTU5NTUy.XYbt6w.yE0ueAEnyzrzmXUVWKCf-Dh5Z9g",
+    "token": "",
     "prefix": ".",
     "commands": [
         "help", "clownrate", "weebrate", "gayrate", "dankrate", "commands", "addsound", "playsound", "delsound", "listsounds", "decide"
