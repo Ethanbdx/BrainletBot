8f0d612660b0b61585d744383fd2051f64a9d3c5
Ethan Boudreaux <ethanbdx999@yahoo.com>
2020-02-19 17:23:31 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let config = {
-    "token": "",
+    "token": "Njc1OTY0MDk3ODQxNDYzMzE2.XkZBnQ.RthcolZGTFqfropFh_7fqAFF2gs",
     "prefix": ".",
     "commands": [
         "help", "clownrate", "weebrate", "gayrate", "dankrate", "commands", "addsound", "playsound", "delsound", "listsounds", "decide", "vibecheck"
