07c90ed41096675bf22c46dec1f15072b69f9943
Ethanbdx <ethanbdx@gmail.com>
2021-07-21 00:21:22 -0500

    oof

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    "token": "Njc1OTY0MDk3ODQxNDYzMzE2.XmAI-g.N2P7U6R6cui3bJ0dgDc7eAEl5Aw",
-    "dbPath": "./brainlet.db",
-    "guildId": "697943683621716118",
-    "botChannel": "697943684087545872"
-}
